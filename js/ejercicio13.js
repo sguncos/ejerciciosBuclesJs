35fdf06f47dfa40be679765d631cfe9468215fde
Sebastián Uncos <suncos.empleo@gmail.com>
2021-02-19 23:04:53 -0300

    se resolvio el ejercicio 13

---
+++ js/ejercicio13.js
@@ -0,0 +1,6 @@
+/*13- Realiza un script que pida un texto y lo muestre en mayúsculas.*/
+
+let texto = prompt("Ingrese un texto:")
+
+texto = texto.toUpperCase();
+document.write(`El texto ingresado es: <br> ${texto}`);
